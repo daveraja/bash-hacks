5eec68be6f94ae79fe7fb354d70edb6a6fa7820a
David Rajaratnam <daver@cse.unsw.edu.au>
2015-06-02 10:31:12 +1000

    Some emacs workspace changes that I made but can't remember what for :(

--- workspaces_emacs.sh
+++ workspaces_emacs.sh
@@ -1,15 +1,15 @@
 #-------------------------------------------------------------------------
 # workspaces_emacs.sh
 #
-# Functions to extend the workspaces stuff to make using emacs server 
+# Functions to extend the workspaces stuff to make using emacs server
 # and client easier. The basic idea is that each workspace runs its own
 # emacs server. When all instances of a workspace are shutdown the
 # associated emacs server is also shutdown.
-# 
+#
 # workspaces_emacs registers itself with the workspaces on_exit hook so
 # that the shutdown check is always checked on exiting a workspace.
 #
-# Note on hackiness: I don't know how portable any of this is across 
+# Note on hackiness: I don't know how portable any of this is across
 # other platforms.
 #
 # Some issues:
@@ -18,7 +18,7 @@
 # command like:
 #
 #    emacsclient -c -n -a "" -s myserver
-# 
+#
 # What I would expect this to do is to try and connect to the emacs
 # server daemon named "myserver" and if it is not running spawn a
 # server with that name and connect to it. Instead, if the server is
@@ -26,14 +26,14 @@
 # connect to it because it has the wrong name.
 #
 # The wkspe_shutdown function needs work. It always prompts the user that
-# there are "active clients". Searching google there are various discussions 
+# there are "active clients". Searching google there are various discussions
 # on what this means and how to get around it. Need to look into it.
 #
 #-------------------------------------------------------------------------
 mbimport workspaces
 
 #------------------------------
-# returns true if a workspace is loaded and false otherwise. If false 
+# returns true if a workspace is loaded and false otherwise. If false
 # writes out an error message.
 #------------------------------
 _wkspe_check_ws_loaded(){
@@ -46,7 +46,7 @@ _wkspe_check_ws_loaded(){
 
 #------------------------------
 # Check if the running emacs server has a frame (ie. a window or terminal in non-emacs speak).
-# Note: for some reason the frame-list returns 1 if there are no frames not 0. Is this 
+# Note: for some reason the frame-list returns 1 if there are no frames not 0. Is this
 # a bug or a property of the emacs server?
 #------------------------------
 _wkspe_has_frame(){
@@ -80,9 +80,9 @@ _wkspe_kill_server(){
     local comm="emacs --daemon=$WORKSPACE_ID"
     local res=$(pgrep -f "$comm")
     [ "$res" == "" ] && return 0
-    kill $res   
+    kill $res
     res=$(pgrep -f "$comm")
-    [ "$res" != "" ]   
+    [ "$res" != "" ]
 }
 
 #------------------------------
@@ -109,13 +109,13 @@ wkspe_shutdown(){
 # (if it is running).
 #------------------------------
 wkspe_on_exit(){
-    # if no more active pids for the current workspace 
+    # if no more active pids for the current workspace
     # then shutdown emacs server.
     local npids=$(wksps_num_active_pids)
     if [ $npids -eq 0 ] && _wkspe_server_isrunning ; then
 	echo "Shutting down emacs server for workspace..."
-	wkspe_shutdown 
-    fi 
+	wkspe_shutdown
+    fi
 }
 
 #------------------------------
@@ -168,12 +168,62 @@ wkspe_emacsclient_nw(){
     fi
 }
 
+#------------------------------
+# Emacs workspace - run ediff from the command-line
+#------------------------------
+wkspe_emacsclient_ediff(){
+    local cmd="(ediff-files \"$1\" \"$2\")"
+    if [ "$WORKSPACE_ID" == "" ]; then
+	echo "error: cannot connect to emacs workspace server as no workspace is currently loaded"
+	return 1
+    fi
+    if [ "$1" == "" ] || [ "$2" == "" ]; then
+	echo "error: no files specified"
+	return 1
+    fi
+    # make sure emacs workspace server is running
+    ! _wkspe_server_isrunning && _wkspe_run_server
+    if ! _wkspe_server_isrunning; then
+	echo "error: failed to run emacs server"
+	return 1
+    fi
+
+    # Now run the ediff
+    if [ "$DISPLAY" == "" ]; then
+	emacsclient -t -s "$WORKSPACE_ID" -e '$cmd'
+    elif _wkspe_has_frame; then
+#	echo "Running1: emacsclient -n -s $WORKSPACE_ID -e '$cmd'"
+	emacsclient -n -s "$WORKSPACE_ID" -e "$cmd"
+    else
+#	echo "Running2: emacsclient -c -n -s $WORKSPACE_ID -e '$cmd'"
+	emacsclient -c -n -s "$WORKSPACE_ID" -e "$cmd"
+    fi
+}
+
+_wkspe_emacsclient_ediff_autocomplete () {
+    local suggestions
+    local cur cmd
+
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    if [ $COMP_CWORD -eq 1 ] || [ $COMP_CWORD -eq 2 ]; then
+	suggestions=$(ls)
+    fi
+    COMPREPLY=( $(compgen -W "${suggestions}" -- ${cur}) )
+    return 0
+}
+
 #-----------------------------------------------------------------------
-# Main - Setup the workspace callback for on_enter and on_exit. 
+# Main - Setup the workspace callback for on_enter and on_exit.
 #-----------------------------------------------------------------------
 
 wksps_hook_on_enter "wkspe_on_enter"
 wksps_hook_on_exit "wkspe_on_exit"
+
+#---------------------------------------------------------------
+# Register the completion functions
+#---------------------------------------------------------------
+complete -F _wkspe_emacsclient_ediff_autocomplete wkspe_emacsclient_ediff
 
 
 export -f _wkspe_check_ws_loaded
@@ -184,4 +234,3 @@ export -f _wkspe_has_frame
 export -f wkspe_shutdown
 export -f wkspe_emacsclient
 export -f wkspe_emacsclient_nw
-
